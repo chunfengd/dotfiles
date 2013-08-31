21f2d3d12da0d7fd3e20e8540fea9b4d28175082
chunfeng <chunfengd@gmail.com>
2013-08-31 15:58:29 -0400

    add key binding for mac

--- init.sh
+++ init.sh
@@ -10,12 +10,22 @@ do
         echo "backup $file to $file.old"
         mv -f $file $file.old
     fi
-
-    if [ -h $file ]; then
-        echo "remove link $file"
-        rm -f $file
-    fi
-
     echo "link $file to $PWD/$f"
     ln -s $PWD/$f $file
 done
+
+# mac keybindings
+if [ $(uname) == 'Darwin' ]; then
+    BINDING_PATH=$HOME/Library/KeyBindings
+    BINDING_FILE=$BINDING_PATH/DefaultKeyBinding.dict
+    if [ ! -d $BINDING_PATH ]; then
+        echo "mkdir $BINDING_PATH"
+        mkdir $BINDING_PATH
+    fi
+    if [ -e $BINDING_FILE ]; then
+        echo "backup $BINDING_FILE"
+        mv -f $BINDING_FILE $BINDING_FILE.old
+    fi
+    echo "link $BINDING_FILE to $PWD/DefaultKeyBinding.dict"
+    ln -s $PWD/DefaultKeyBinding.dict $BINDING_FILE
+fi
