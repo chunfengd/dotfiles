f1855ffe7a577406d0165f1db292bbefe52c0fec
Chunfeng Dai <4458219+chunfengd@users.noreply.github.com>
2023-01-19 13:05:09 -0500

    unhidden source files

--- init.sh
+++ init.sh
@@ -1,32 +1,55 @@
 #!/bin/bash
 
-fs=".bash_aliases .inputrc  .zsh_aliases .profile"
-fs=$fs" git-completion.bash git-prompt.sh .bash_git git-completion.zsh .zsh_git"
 t=`date +%s`
+backup_dir="$HOME/.backup"
+dst_dir=$HOME
 
-# backup old files if exist
-for f in $fs
-do
-    file=$HOME/$f
+pairs=()
+pairs+=("bash_aliases .bash_aliases")
+pairs+=("inputrc .inputrc")
+pairs+=("zsh_aliases .zsh_aliases")
+pairs+=("profile .profile")
+
+# git
+pairs+=("git-completion.bash git-completion.bash")
+pairs+=("git-prompt.sh git-prompt.sh")
+pairs+=("bash_git .bash_git")
+pairs+=("git-completion.zsh git-completion.zsh")
+pairs+=("zsh_git .zsh_git")
+
+if [ ! -d $BINDING_PATH ]; then
+    echo "create backcup dir: $backup_dir"
+    mkdir -p $backup_dir
+fi
+
+for i in ${!pairs[@]}; do
+    pair=(${pairs[$i]})
+    src=${pair[0]}
+    dst=${pair[1]}
+    # echo $src $dst
+    file=$dst_dir/$dst
     if [ -e $file ]; then
-        echo "backup $file to $file.old"
-        cp $file{,.$t}
+        backup_file=$backup_dir/$src.$t
+        echo "backup $file to $backup_file"
+        cp $file $backup_file
     fi
-    echo "link $file to $PWD/$f"
-    ln -sf $PWD/$f $file
+    echo "link $file to $PWD/$src"
+    # TODO
+    # ln -sf $PWD/$src $file
 done
 
 # mac keybindings
 if [ $(uname) == 'Darwin' ]; then
     BINDING_PATH=$HOME/Library/KeyBindings
     BINDING_FILE=$BINDING_PATH/DefaultKeyBinding.dict
+    BACKUP_FILE=$backup_dir/DefaultKeyBinding.dict.$t
     if [ ! -d $BINDING_PATH ]; then
         echo "mkdir $BINDING_PATH"
         mkdir $BINDING_PATH
     fi
     if [ -e $BINDING_FILE ]; then
-        echo "backup $BINDING_FILE"
-        cp $BINDING_FILE{,.$t}
+        echo "backup $BINDING_FILE to $BACKUP_FILE"
+        cp $BINDING_FILE $BACKUP_FILE
     fi
     echo "link $BINDING_FILE to $PWD/DefaultKeyBinding.dict"
     ln -f $PWD/DefaultKeyBinding.dict $BINDING_FILE
